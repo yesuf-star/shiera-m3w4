57627eebc549ba0c9f5cbc4e0d3979551a44156d
yesuf-star <ibnuesmael2022@gmail.com>
2026-09-07 08:37:21 +0300

    setup linting formatting and github actions

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+export default defineConfig(
+  {
+    ignores: ['dist/**'],
+  },
+
+  {
+    files: ['**/*.{js,ts}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+  },
+);
